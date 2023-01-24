60f1a2575f4d015caa81969393a568c2762bc071
jmce619 <jmcelhiney619@gmail.com>
2023-01-23 20:56:46 -0500

    adding all files

---
+++ ecommerce/ecommerce/components/HeroBanner.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Link from 'next/link'
+
+export const HeroBanner = () => {
+  return (
+    <div className='hero-banner-container'>
+        <div>
+            <p className="mini-bike">SMALL TEXT</p>
+            <h3>MID TEXT</h3>
+            <img src="" alt="mini-bike" className="hero-banner-image"/>
+            
+            <div><Link href="/product/ID">
+                <button type="button">BUTTON TEXT</button>
+            </Link>
+            </div>
+            <div className="desc">
+                <h5>Description</h5>
+                <p>DESCRIPTION HERE</p>
+            </div>
+            </div>
+            <div></div>
+            </div>
+
+  )
+}
+
+export default HeroBanner
---
+++ ecommerce/pages/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {client} from '../lib/client';
+import {Product, FooterBanner, HeroBanner} from '../ecommerce/components';
+
+const Home = () => {
+  return (
+    <div>
+      <HeroBanner/>
+    
+    <div className="products-heading">
+      <h2> Best Selling Products</h2>
+      <p> Speakers of many variations</p>
+    </div>
+    <div className="products-container">
+      {['Product 1', 'Product 2'].map(
+        (product) => product)}
+      
+    </div>
+    </div>
+
+  )
+}
+
+export default Home;
