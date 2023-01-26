1981453d635adc564b665088991bb993d1399842
jmce619 <jmcelhiney619@gmail.com>
2023-01-25 22:19:49 -0500

    generate images for product

--- ecommerce/ecommerce/components/Product.jsx
+++ ecommerce/ecommerce/components/Product.jsx
@@ -8,7 +8,7 @@ const Product = ({product: {image,name,slug,price}}) => {
         <Link href = {`product/${slug.current}`}>
             <div className = "product-card">
                 <img 
-                src={urlFor(image && image[0])}
+                src={urlFor(image[0])}
                 width={250}
                 height={250}
                 className = "product-image"
@@ -20,3 +20,5 @@ const Product = ({product: {image,name,slug,price}}) => {
     </div>
   )
 }
+
+export default Product
