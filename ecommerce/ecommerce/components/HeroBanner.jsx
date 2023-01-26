adeb3f43e1b93b58116a02f31f20a328271faff4
jmce619 <jmcelhiney619@gmail.com>
2023-01-25 21:50:59 -0500

    upload Main banner and footer banner

--- ecommerce/ecommerce/components/HeroBanner.jsx
+++ ecommerce/ecommerce/components/HeroBanner.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
-import Link from 'next/link'
+import React from 'react';
+import Link from 'next/link';
 
-export const HeroBanner = () => {
+import { urlFor } from '../../lib/client';
+
+const HeroBanner = ({ heroBanner }) => {
   return (
-    <div className='hero-banner-container'>
-        <div>
-            <p className="mini-bike">SMALL TEXT</p>
-            <h3>MID TEXT</h3>
-            <img src="" alt="mini-bike" className="hero-banner-image"/>
-            
-            <div><Link href="/product/ID">
-                <button type="button">BUTTON TEXT 3</button>
-            </Link>
-            </div>
-            <div className="desc">
-                <h5>Description</h5>
-                <p>DESCRIPTION HERE</p>
-            </div>
-            </div>
-            <div></div>
-            </div>
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
+        <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
 
+        <div>
+          <Link href={`/product/${heroBanner.product}`}>
+            <button type="button">{heroBanner.buttonText}</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>{heroBanner.desc}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default HeroBanner
+export default HeroBanner;
