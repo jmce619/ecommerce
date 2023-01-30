5020f037231e73064ff3b472a9cbd8e37e74ca58
jmce619 <jmcelhiney619@gmail.com>
2023-01-29 22:35:01 -0500

    refresh

--- ecommerce/pages/_app.js
+++ ecommerce/pages/_app.js
@@ -1,18 +1,5 @@
-import React from 'react';
-import { Toaster } from 'react-hot-toast';
+import '@/styles/globals.css'
 
-import { Layout } from '../ecommerce/components';
-import { StateContext } from '../ecommerce/context/StateContext';
-
-function MyApp({ Component, pageProps }) {
-  return (
-    <StateContext>
-      <Layout>
-        <Toaster />
-        <Component {...pageProps} />
-      </Layout>
-    </StateContext>
-  )
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
-
-export default MyApp
---
+++ ecommerce/sanityecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'nhooots7',
+    dataset: 'production'
+  }
+})
