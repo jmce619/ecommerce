de0c361713147f20456ff9d4ce2b051947a38039
jmce619 <jmcelhiney619@gmail.com>
2023-01-31 23:08:07 -0500

    add imageurlbuilder

--- ecommerce/lib/client.js
+++ ecommerce/lib/client.js
@@ -1,14 +1,15 @@
 import sanityClient from '@sanity/client'
 import { ImageUrlBuilder } from 'next-sanity-image'
 
-export const client = sanityClient({
-    projectId: 'vfxfwnaw',
+
+ const client = sanityClient({
+    projectId: 'nhooots7',
     dataset: 'production',
     apiVersion: '2022-03-10',
     useCdn: true,
     token: process.env.NEXT_PUBLIC_SANITY_TOKEN
   });
-  
+
   const builder = ImageUrlBuilder(client);
-  
+
   export const urlFor = (source) => builder.image(source);
