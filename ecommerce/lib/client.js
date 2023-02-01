3a6ce74dbca8c07dd3c30166dbdffffa6e1f7da5
jmce619 <jmcelhiney619@gmail.com>
2023-01-31 22:31:47 -0500

    add client js

---
+++ ecommerce/lib/client.js
@@ -0,0 +1,14 @@
+import sanityClient from '@sanity/client'
+import { ImageUrlBuilder } from 'next-sanity-image'
+
+export const client = sanityClient({
+    projectId: 'vfxfwnaw',
+    dataset: 'production',
+    apiVersion: '2022-03-10',
+    useCdn: true,
+    token: process.env.NEXT_PUBLIC_SANITY_TOKEN
+  });
+  
+  const builder = ImageUrlBuilder(client);
+  
+  export const urlFor = (source) => builder.image(source);
--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Product, FooterBanner, HeroBanner } from '../components';
 
-const index = () => {
-  return (
-    <div>index</div>
-  )
-}
 
-export default index
+const Home = ({ products, bannerData }) => (
+  <div>
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
+    <div className="products-heading">
+      <h2>Best Seller Products</h2>
+      <p>speaker There are many variations passages</p>
+    </div>
+
+    <div className="products-container">
+      {products?.map((product) => <Product key={product._id} product={product} />)}
+    </div>
+
+    <FooterBanner footerBanner={bannerData && bannerData[0]} />
+  </div>
+);
+
+export default Home
